417a3801dc12940b2ae1c38cdb248702b0b22452
Alec Jasik <alec_jasik@yahoo.com>
2020-08-14 14:19:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word (string)
   string_array = string.split 
   reverse_array = []
-  string_array.each do |word|
+  string_array.collect do |word|
     reverse_array << word.reverse 
   end 
   reverse_array.join(" ")
